213f0cd4deadfa8ebb489c1995eb5a70548be2da
OUMAIMA ZYANI <65038328+zyani123@users.noreply.github.com>
2023-12-05 21:07:58 +0100

    Update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# Using Puppet, create a file in '/tmp'.
+# creates a file in /tmp.
+
 file { '/tmp/school':
-  ensure  => 'file',
-  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
